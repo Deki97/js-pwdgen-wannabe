c4f892b3f56c1fb399a99ed27c20771ede64bc1a
Deki97 <dejan97@libero.it>
2021-11-11 15:48:02 +0100

    done

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@
 // poi chiedi il suo cognome,
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+// Inserimento Nome
+const userName = prompt('Inserisci il tuo nome:');
+console.log(userName);
+
+
+// Inserimento Cognome
+const userSurname = prompt('Inserisci il tuo cognome:');
+console.log(userSurname);
+
+
+// Inserimento Colore Preferito
+const favoriteColor = prompt('Inserisci il tuo colore preferito:');
+console.log(favoriteColor);
+
+
+// Concatenazione Stringhe (Generazione Password Sicurissima)
+const password = userName + userSurname + favoriteColor + 21;
+
+
+// Output nel tag html <h1>
+document.getElementById('passwordGenerata').innerHTML = 'La password generata è : ' + password;
